ac81352768accfb0b684b824dd1bef601ceaccd7
pravanjan <pravanjan.niranjan@a-cti.com>
2018-07-11 19:37:25 +0530

    Test for private method using reflection

--- src/test/java/demo/DemoMockTest.java
+++ src/test/java/demo/DemoMockTest.java
@@ -6,6 +6,7 @@ import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
+import java.lang.reflect.Method;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,9 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import junitx.util.PrivateAccessor;
+
 import static org.mockito.Mockito.*;
 
 
@@ -39,6 +43,18 @@ public class DemoMockTest {
 		 DemoMock demoMockSpy = spy(new DemoMock());
 		 PowerMockito.doReturn("calling public").when(demoMockSpy).iampublic(anyString());
 		 assertEquals(demoMockSpy.callingPublicMethodInside("pravanjan"),"calling public");
+	  }
+	 
+	 
+	 @Test
+	 public void iamPrivateTest() throws Exception {
+		 DemoMock demo = new DemoMock();
+		 Method privateMethod = DemoMock.class.getDeclaredMethod("iamPrivate", String.class);
+		 privateMethod.setAccessible(true);
+		 assertEquals(privateMethod.invoke(demo,"hi"),"hi called private ");
+
+		 
+		
 	  }
 
 	
